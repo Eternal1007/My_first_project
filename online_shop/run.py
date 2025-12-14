a5af3cd8fdd6339f2da202cc8cc144f3ea47016a
Kostya <kostyakarpa6@gmail.com>
2025-12-14 10:36:09 +0200

    Many changes)

--- online_shop/run.py
+++ online_shop/run.py
@@ -6,7 +6,7 @@ import os
 app = create_app()
 
 if __name__ == '__main__':
-    #Create DB tables if not exist
+    #Create DB tables if not exist.
     with app.app_context():
         db.create_all()
     app.run(debug=True)
