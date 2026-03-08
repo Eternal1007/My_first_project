bdead34e22c3ccca678aacc2b38399ff6f2b7c7f
Kostya <kostyakarpa6@gmail.com>
2026-03-08 08:57:56 +0200

    Final poll

--- online_shop/run.py
+++ online_shop/run.py
@@ -10,6 +10,10 @@ if __name__ == '__main__':
     with app.app_context():
         db.create_all()
     app.run(debug=True)
+    
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
+
     #if __name__ == '__main__': - перевіряє, чи запускається скрипт безпосередньо
     #with app.app_context(): - створює контекст додатка для роботи з базою даних
     #db.create_all() - створює всі таблиці в базі даних, визначені моделями SQLAlchemy
